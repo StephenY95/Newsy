f1594d9d8364cdd2b47762efb9260392f7412c6d
Stephen Yao <stephenyao95@gmail.com>
2020-04-19 16:26:50 -0400

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2n9sjIg9fQ_mkK8E7VDpgFvyh9Fpx4wY",
+    authDomain: "newsy-8be12.firebaseapp.com",
+    databaseURL: "https://newsy-8be12.firebaseio.com",
+    projectId: "newsy-8be12",
+    storageBucket: "newsy-8be12.appspot.com",
+    messagingSenderId: "392305018584",
+    appId: "1:392305018584:web:f85f6d260d2910f6a979ad",
+    measurementId: "G-2NF9WQX35Z"
+  };
+
+  export default firebaseConfig
